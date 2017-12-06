4d0231324d7f692ca117574909ba84d80eedd4d1
JulianNobis <31952254+JulianNobis@users.noreply.github.com>
2017-12-06 16:09:34 +0100

    optimized the style of 'parent of pat'

--- genealogy.pl
+++ genealogy.pl
@@ -1,9 +1,4 @@
 /* parent and grandparent have an arity of 2, male and female have an arity of 1 */
-parent/2.
-male/1.
-female/1.
-grandparent/2.
-
 
 parent(adam,john).
 parent(eve,john).
@@ -13,9 +8,9 @@ parent(john,pat).
 parent(pat,jacob).
 parent(carol, jacob).
 
-isParent(X,pat) :- parent(X,pat). /*isParent(X,pat). --> now you know who the parent of pat is (it's john) */
- 
-isLisaParent(lisa,X) :- parent(lisa,X). /*isLisaParent(lisa,X). --> finds out whether lisa has a child or not (no she does not have on) */
+
+whoIsTheParentOfPat(X) :- parent(X,pat).
+isLisaAParent(X) :- parent(lisa,X).
 
 male(adam).
 male(john).
@@ -29,4 +24,4 @@ grandparent(adam,anne).
 grandparent(eve,anne).
 grandparent(john,jacob).
 
-grandparent(GP,GC) :- parent(C,GC), parent(GP,C). /* parent(C,GC)... C is parent of GC; parent(GP,C) ... GP is parent of C*/
+grandparent(GP,GC) :- parent(C,GC), parent(GP,C). % parent(C,GC)... C is parent of GC; parent(GP,C) ... GP is parent of C
